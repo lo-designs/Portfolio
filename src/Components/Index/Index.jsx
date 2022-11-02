d5bd4bed3911bdfeaff6d53ea98bbcdae9344f13
lo-designs <lauraguntherdesigns@gmail.com>
2022-11-02 13:31:01 -0500

    add home/index page

---
+++ src/Components/Index/Index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import About from "../About/About";
+import Portfolio from "../Portfolio/Portfolio";
+import "./Index.scss";
+
+function Index() {
+  return (
+    <div className="home-page">
+      <div className="main-container">
+        <h1 className="design">
+          front end design<br></br>& development
+        </h1>
+      </div>
+      <div className="portfolio-section">
+        <Portfolio />
+      </div>
+      <div className="about-section">
+        <About />
+      </div>
+    </div>
+  );
+}
+
+export default Index;
