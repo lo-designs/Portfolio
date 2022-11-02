c826214d71a301c6c5ae77d468c976c35fe35658
lo-designs <lauraguntherdesigns@gmail.com>
2022-11-02 13:30:36 -0500

    add header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <span className="header-links">
+        <a className="header-logo" href="/">
+          laura gunther
+        </a>
+        <span className="header-links-divider"></span>
+        <a className="portfolio" href="/portfolio">
+          portfolio
+        </a>
+        <span className="header-links-divider"> // </span>
+        <a className="about" href="/about">
+          about
+        </a>
+      </span>
+    </div>
+  );
+}
+
+export default Header;
