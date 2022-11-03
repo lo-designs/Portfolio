d1274759cb294b17ca88546be80b8933c410692f
lo-designs <lauraguntherdesigns@gmail.com>
2022-11-03 11:40:54 -0500

    add neurotype carousels

---
+++ src/Components/Carousels/NeurotypeCarousel/NeurotypeCarousel3.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./../Carousel.scss";
+
+function NeurotypeCarousel3() {
+   return (
+      <div className="carousel">
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmV0cm98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel-header">First slide label</h3>
+            <p className="carousel-description">
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmV0cm98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3 className="carousel-header">Second slide label</h3>
+            <p className="carousel-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1467991521834-fb8e202c7074?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHJldHJvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3 className="carousel-header">Third slide label</h3>
+            <p className="carousel-description">
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+      </Carousel>
+    </div>
+   )
+}
+
+export default NeurotypeCarousel3;
