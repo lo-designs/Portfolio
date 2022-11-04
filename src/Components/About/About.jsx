501032a2cc5ce8ad7f6f24efc2987888b7410580
lo-designs <lauraguntherdesigns@gmail.com>
2022-11-03 21:43:33 -0500

    add media queries to about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,23 +5,24 @@ import "./About.scss";
 function About() {
   return (
     <div className="about-section">
-      <h1 className="about-heading">How I got into design & development.</h1>
+      <div className="about-summary">
+        <h3 className="about-heading">How I got into design & development.</h3>
 
-      <div className="about-description">
-        <p>
-          A pretty unconventional path lead me from working in the service
-          industry, to studying Psychology, to pursuing a career in UX/UI
-          design. While learning UX, I discovered my love for digital design and
-          development.
-        </p>
+        <div className="about-description">
+          <p>
+            A pretty unconventional path lead me from working in the service
+            industry, to studying Psychology, to pursuing a career in UX/UI
+            design. While learning UX, I discovered my love for digital design
+            and development.
+          </p>
+        </div>
       </div>
-
       <div className="about-me">
-        <img
+        {/* <img
           className="me"
           src="https://media2.giphy.com/media/QSuOxLANMqaU72CHwx/giphy.gif?cid=ecf05e47dp253tdit25l6tzd7bgf7tctmmnag3ahx3e3yvwk&rid=giphy.gif&ct=g"
           alt=""
-        />
+        /> */}
         <p className="favorite-things">
           Aside from digital design & development, some of my favorite things
           include.*✧
@@ -32,7 +33,6 @@ function About() {
           pancakes.
         </p>
       </div>
-
     </div>
   );
 }
