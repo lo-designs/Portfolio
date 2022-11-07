fa96c0936745d1718f022b99b4bf12983a21fc04
lo-designs <lauraguntherdesigns@gmail.com>
2022-11-07 09:41:23 -0600

    add (start of) dark mode component

---
+++ src/Components/DarkMode/DarkMode.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import "./DarkMode.scss";
+
+const DarkMode = () => {
+   return (
+      <div className="dark-mode">
+         <button className="dark">// dark</button>
+         
+      </div>
+   )
+}
+
+export default DarkMode;
