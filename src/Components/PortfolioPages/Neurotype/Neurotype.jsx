55bf463c35fc26c553335958b828fa19f9bd343f
lo-designs <lauraguntherdesigns@gmail.com>
2023-01-15 14:28:30 -0600

    Update Neurotype.jsx

--- src/Components/PortfolioPages/Neurotype/Neurotype.jsx
+++ src/Components/PortfolioPages/Neurotype/Neurotype.jsx
@@ -51,7 +51,7 @@ function Neurotype() {
                 <div id="title">Neurotype</div>
                 <div className="project-type">Medical Startup // UX UI</div>
                 <div className="subtitle">
-                  App prototype for an early stage medical startup.
+                  App prototype for a medical startup aiming to help treat addiciton.
                 </div>
 
                 <div className="overview">
