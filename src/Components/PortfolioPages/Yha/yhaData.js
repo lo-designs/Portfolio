d2cf355b2a495494609b8ae4836b1ed9dee78d1a
lo <80482085+lo-designs@users.noreply.github.com>
2026-01-15 15:37:53 -0600

    yha update

--- src/Components/PortfolioPages/Yha/yhaData.js
+++ src/Components/PortfolioPages/Yha/yhaData.js
@@ -10,26 +10,26 @@ const yhaData = {
   projectType: "Self-defined project // UX UI",
   subtitle: "Researching and protoyping a health & wellness resource app.",
   overview: [
-    "While at Prime, my team had the privilege of working with Yha Inc., a company building portable brain sensing tools to support the recovery of people struggling with Opioid Use Disorder [OUD]. Our task was to help determine the best way to integrate a portable electroencephalogram [EEG] into a standard therapy/clinical setting, as well as create a prototype for NeuromarkR™, an application that displays EEG measurements."
+    "After an internship at The Bridge for Youth, a nonprofit in Minneapolis that provides shelter and necessities for displaced youth, I wanted to design a mobile app for youth in crisis to find and connect with support specific to their needs."
   ],
   tabs: [
     {
       label: "Initial Research",
-      image: yhaMap,
+      image: yhaResearch,
       titleNumber: "Secondary Research // Informational Interviews",
       descriptionTitle: "01 Existing health & wellness resources for youth",
       paragraphs: [
-        "In researching existing resources, I found that while there are a lot of them, many of are text heavy and hard to scan for important information; making it difficult to find help, especially if you don't know exactlywhat you need. I used YSN (Youth Services Network) as interviews with licensed social workers revealed it to be an effective (but outdated) resource."
+        "The challenge was to learn about the resources for youth that currently exist to find areas of opportunity, and from there determine a way to make these resources easy to find and access. In my research, I found that many resources were outdated or you had to dig to find important information; making them overall difficult or inaccessible. Interviews with licensed social workers revealed that YSN (Youth Services Network) was an effective (but outdated) resourcem so I decided to use that as a starting point."
       ]
     },
     {
       label: "Determining Features",
-      image: yhaResearch,
+      image: yhaMap,
       imageCaption: "[ Experience map: made in Sketch ]",
       titleNumber: "Proto-persona // Experience mapping // User flow",
       descriptionTitle: "02 Determining how to address health & wellness needs.",
       paragraphs: [
-        "The interviews, research, and analysis of YSN helped me determine features for the app: easy to access contact info, scannable information, direct call and email links. Additionally, I wanted to implement a way to narrow down resources to find those that best fit the needs of the person using the app. I created a proto-persona, an experience map, auser flow, and wireframes using Sketch and Adobe XD."
+        "The interviews, research, and analysis of YSN helped me determine features for the app: easy to access contact info, scannable information, direct call and email links. Additionally, I wanted to implement a way to narrow down resources to find those that best fit the needs of the person using the app. I created a proto-persona, an experience map, a user flow, and wireframes using Sketch and Adobe XD."
       ]
     },
        {
