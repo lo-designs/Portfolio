007fbf8a782344d1331e3c7547265dcade03357e
lo <80482085+lo-designs@users.noreply.github.com>
2026-01-15 15:18:26 -0600

    fix header/footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer({ colorTheme }) {
   return (
     <footer className={`footer ${colorTheme}`}>
       <span className="footer-links">
-        <a className="footer-resume-link" href="/">
-          resume
+        <a className="footer-resume-link" href="/site-res.pdf" target="_blank" rel="noopener noreferrer">
+          resume 
         </a>
         <span className="footer-links-divider"> // </span>
         <a className="footer-email-link" href="mailto:laura.e.gunther@gmail.com">
