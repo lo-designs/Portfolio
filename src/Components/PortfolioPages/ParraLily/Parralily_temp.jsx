7a5a1bfb2606d27e8f068c4d4cbec326a6c3dc9a
lo <80482085+lo-designs@users.noreply.github.com>
2026-01-15 17:20:11 -0600

    Delete Parralily_temp.jsx

--- src/Components/PortfolioPages/ParraLily/Parralily_temp.jsx
+++
@@ -1,8 +0,0 @@
-import PortfolioPageLayout from "../PortfolioPageLayout";
-import parralilyData from "./parralilyData";
-
-function Parralily() {
-  return <PortfolioPageLayout {...parralilyData} />;
-}
-
-export default Parralily;
