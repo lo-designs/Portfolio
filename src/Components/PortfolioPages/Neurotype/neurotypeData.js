11611b8c28bfc2906277f85b772adb3719face9b
lo <80482085+lo-designs@users.noreply.github.com>
2026-01-16 01:17:45 -0600

    mobile fix >?

--- src/Components/PortfolioPages/Neurotype/neurotypeData.js
+++ src/Components/PortfolioPages/Neurotype/neurotypeData.js
@@ -37,7 +37,7 @@ const neurotypeData = {
       ]
     },
       {
-      label: "Data Visualization",
+      label: "Data Vis",
       image: neuroDash,
       imageCaption: "[ Data visualization: made in Figma ]",
       titleNumber: "Data Visualization // Prototyping",
