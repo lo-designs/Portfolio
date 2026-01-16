7b1bd86036b57a3a992fb6fc873e256fae362c53
lo <80482085+lo-designs@users.noreply.github.com>
2026-01-15 19:12:17 -0600

    update portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,23 +6,20 @@ import yhaHover from "../Images/Yha/hover.jpg";
 import neuroHover from "../Images/Neurotype/hover.jpg";
 import soonaHover from "../Images/Soona/hover.jpg";
 
-
 function Portfolio() {
   return (
     <div className="portfolio">
       <div className="portfolio-intro">
-        <h3 className="portfolio-header">// PORTFOLIO</h3>
+        <h3 className="portfolio-header">PORTFOLIO</h3>
         <h3 className="portfolio-subheader">
-          Web design & development work + UX UI design case studies. 
+          Web design & development work + UX UI design case studies.
         </h3>
       </div>
       <nav className="menu">
         <div className="menu__item">
           <a id="outta" href="/outtawax" className="menu__item-link">
             Outta Wax
-            <p className="work__type">
-              [COMING SOON!] vinyl pressing services // web design
-            </p>
+            <p className="work__type">vinyl pressing services | web design</p>
           </a>
           <img
             className="menu__item-img"
@@ -32,8 +29,8 @@ function Portfolio() {
           <div className="marquee">
             <div id="outta-marquee" className="marquee__inner">
               <span>Outta Wax</span>
-              <span>// Vinyl Pressing Services</span>
-              <span>// Web Design</span>
+              <span>|</span>
+              <span>Web Design</span>
             </div>
           </div>
         </div>
@@ -59,31 +56,31 @@ function Portfolio() {
           <a id="pettes" href="/petteshounds" className="menu__item-link">
             Pette's Hounds
             <p className="work__type">
-              dog walking & pet care services // web [re]design
+              dog walking & pet care services | web redesign
             </p>
           </a>
           <img className="menu__item-img" src={pettesHover} alt="" />
           <div className="marquee">
             <div id="pettes-marquee" className="marquee__inner">
               <span>Pette's Hounds</span>
-              <span>// Web Design</span>
-              <span>// Pet Care Services.</span>
+              <span>|</span>
+              <span>Web Redesign</span>
             </div>
           </div>
         </div>
-    
+
         <hr id="portfolio-line"></hr>
         <div className="menu__item">
           <a id="neuro" href="/neurotype" className="menu__item-link">
             Neurotype
-            <p className="work__type">medical startup // ux ui design</p>
+            <p className="work__type">medical startup | ux ui design</p>
           </a>
           <img className="menu__item-img" src={neuroHover} alt="" />
           <div className="marquee">
             <div id="neuro-marquee" className="marquee__inner">
               <span>Neurotype</span>
-              <span>// Medical Startup</span>
-              <span>// UX UI Design</span>
+              <span>|</span>
+              <span>UX UI Design</span>
             </div>
           </div>
         </div>
@@ -92,7 +89,7 @@ function Portfolio() {
         <div className="menu__item">
           <a id="soona" href="/soona" className="menu__item-link">
             Soona
-            <p className="work__type">24 hr content studio // ux ui design</p>
+            <p className="work__type">24 hr content studio | ux ui design</p>
           </a>
           <img className="menu__item-img" src={soonaHover} alt="" />
           <div className="marquee">
@@ -102,25 +99,26 @@ function Portfolio() {
               aria-hidden="true"
             >
               <span>Soona</span>
-              <span>// 24hr Content Studio</span>
-              <span>// UX UI Design</span>
+              <span>|</span>
+              <span>UX UI Design</span>
             </div>
           </div>
         </div>
-        
+
         <hr id="portfolio-line"></hr>
         <div className="menu__item">
           <a id="yha" href="/yha" className="menu__item-link">
             Health & Wellness App
             <p className="work__type">
-              Self-defined project // web + ux ui design
+              Self-defined project | web + ux ui design
             </p>
           </a>
           <img className="menu__item-img" src={yhaHover} alt="" />
           <div className="marquee">
             <div id="yha-marquee" className="marquee__inner" aria-hidden="true">
-              <span>// Self-Defined Project</span>
-              <span>// Web + UX UI Design.</span>
+              <span>Health & Wellness App</span>
+              <span>|</span>
+              <span>Web + UX UI Design.</span>
             </div>
           </div>
         </div>
